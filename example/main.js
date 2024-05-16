e23a582adc98006f0dc9c7309e90efd38b5f8db4
mezacosiojuanpablo <mezacosiojuanpablo@gmail.com>
2024-05-16 18:06:22 +0000

    Primer ejemplo usando IDX

---
+++ example/main.js
@@ -0,0 +1,9 @@
+console.log("Hola")
+console.log("Bienvenidos a la materia de principios de programacion logica")
+
+/*
+-- Pasos para mandar cambios a repositorio desde la terminal 
+1.-git add
+2.-git commit -m "Mensaje de lo que mandas a repo"
+3.-git push origin master 
+*/
